f95efa80f6d2a7fcd0af80d78b07e092caca5720
rohan9932 <rohanrahim024@gmail.com>
2026-06-20 01:30:07 +0600

    added leaderboard to public page

---
+++ src/app/(public)/leaderboard/page.tsx
@@ -0,0 +1,9 @@
+import LeaderboardPage from "@/components/pages/LeaderboardPage";
+
+export default function Page() {
+  return (
+    <div className="pt-24 pb-16 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto min-h-screen">
+      <LeaderboardPage />
+    </div>
+  );
+}
